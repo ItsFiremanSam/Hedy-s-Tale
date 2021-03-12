df517b3162aeedf6c4fce2d505d61b8a207b1b6e
Jasper Veldhuizen <misterjacket@hotmail.com>
2021-03-12 19:30:05 +0100

    Added main menu & flowers/mushroom assets & font & prefab of main camera

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
