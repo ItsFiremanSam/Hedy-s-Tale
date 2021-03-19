8f378614e0befb69f8ec242f70fee47c37432909
VNugraha <18079423@student.hhs.nl>
2021-03-19 11:02:11 +0100

    Hedy can interact with interactible objects

---
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private bool _inTrigger;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _inTrigger)
+        {
+            Debug.Log("Interacting with an InteractibleOjbect!");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("InteractibleObject"))
+        {
+            _inTrigger = true;
+            Debug.Log("Entering trigger:" + collision.name);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("InteractibleObject"))
+        {
+            _inTrigger = false;
+            Debug.Log("Exiting trigger:" + collision.name);
+        }
+    }
+}
