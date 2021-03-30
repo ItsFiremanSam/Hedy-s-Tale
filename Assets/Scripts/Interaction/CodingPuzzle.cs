9ab66af25fd2ea0157cced6d76d726d6c825737f
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2021-03-30 20:05:53 +0200

    Integrated code with inventory system

---
+++ Assets/Scripts/Interaction/CodingPuzzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingPuzzle : InteractableObject
+{
+    private CodingUIHandler _codingUIHandler;
+
+    public List<PuzzleBlock> Answer;
+    public string PuzzleDescription;
+
+    public Transform Barrier;
+
+    private void Awake()
+    {
+        _codingUIHandler = FindObjectOfType<CodingUIHandler>();
+    }
+
+    protected override void OnInteractWithPlayer(PlayerInteraction playerInteraction)
+    {
+        _codingUIHandler.ShowCodingUI(playerInteraction.Inventory, Answer, PuzzleDescription, OnPuzzleCompleteCallback);
+    }
+
+    // TODO: Make animation possible using animation waypoint system
+    public void OnPuzzleCompleteCallback()
+    {
+        Debug.Log("OnPuzzleCompleteCallback: " + name);
+        Destroy(Barrier.gameObject);
+    }
+}
