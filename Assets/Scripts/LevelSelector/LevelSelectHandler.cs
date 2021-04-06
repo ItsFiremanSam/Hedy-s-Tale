d507a708be3a1f7debf0deca18f174605d907963
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2021-04-06 14:03:03 +0200

    Added basics for level selector

---
+++ Assets/Scripts/LevelSelector/LevelSelectHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectHandler : MonoBehaviour
+{
+    public int maxLevel;
+    private void OnDrawGizmos()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Transform childTransform = transform.GetChild(i);
+            Rect childRect = childTransform.GetComponent<RectTransform>().rect;
+            GizmosSquare.Draw(childTransform, childRect.width, childRect.height, Color.blue);
+            if (i > 0)
+            {
+                GizmosArrow.Draw(transform.GetChild(i - 1).position, childTransform.position, Color.red, 30, 20);
+            }
+        }
+    }
+}
