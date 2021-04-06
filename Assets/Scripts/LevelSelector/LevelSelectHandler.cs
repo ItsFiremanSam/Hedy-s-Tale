978a8792b2c4796ec61cc6e2e7af6623a6523894
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2021-04-06 18:25:25 +0200

    Tried out selfmade Bezier Curves

--- Assets/Scripts/LevelSelector/LevelSelectHandler.cs
+++ Assets/Scripts/LevelSelector/LevelSelectHandler.cs
@@ -11,12 +11,7 @@ public class LevelSelectHandler : MonoBehaviour
         for(int i = 0; i < transform.childCount; i++)
         {
             Transform childTransform = transform.GetChild(i);
-            Rect childRect = childTransform.GetComponent<RectTransform>().rect;
-            GizmosSquare.Draw(childTransform, childRect.width, childRect.height, Color.blue);
-            if (i > 0)
-            {
-                GizmosArrow.Draw(transform.GetChild(i - 1).position, childTransform.position, Color.red, 30, 20);
-            }
+            if (i > 0) GizmosArrow.Draw(transform.GetChild(i - 1).position, childTransform.position, Color.red, 30, 20);
         }
     }
 }
---
+++ Assets/Scripts/LevelSelector/LevelSignButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelSignButton : MonoBehaviour
+{
+    [HideInInspector]
+    public Path Path;
+
+    public void CreatePath()
+    {
+        Path = new Path(transform.position);
+    }
+}
