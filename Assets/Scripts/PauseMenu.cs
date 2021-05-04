b7809920f9110c64abdb1ed69d4d762149a138e2
Jasper Veldhuizen <misterjacket@hotmail.com>
2021-05-04 16:27:57 +0200

    Added pause menu when pressed escape

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;    
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadSettingsMenu()
+    {
+         
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
