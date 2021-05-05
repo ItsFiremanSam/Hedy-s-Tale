c28fabf100ad680e2c08d69ee648e1f1fe438e07
Jasper Veldhuizen <misterjacket@hotmail.com>
2021-05-05 10:33:54 +0200

    Fixed GetKeyDown(KeyCode) error

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(Keycode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isGamePaused)
             {
