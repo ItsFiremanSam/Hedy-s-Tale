99f62374033eba863c2b389c9bdc5f4c61044646
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2021-05-11 17:29:54 +0200

    Added comment explaining empty MonoBehaviour

--- Assets/Scripts/LevelSelector/CloudContainerScript.cs
+++ Assets/Scripts/LevelSelector/CloudContainerScript.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is necessary for the Custom Editor that is used for the CloudContainer
+/// A Unity Custom Editor needs to be binded to a MonoBehaviour to function
+/// </summary>
 public class CloudContainerScript : MonoBehaviour
 {
 
