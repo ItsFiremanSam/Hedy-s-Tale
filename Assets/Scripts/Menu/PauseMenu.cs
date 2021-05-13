fa874ac04601779e7e8684b09394d40a46f32ff7
Jasper Veldhuizen <misterjacket@hotmail.com>
2021-05-13 21:05:49 +0200

    Removed comment
    Co-authored-by: Lucas Spits <spits.lucas@protonmail.com>

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -11,7 +11,6 @@ public class PauseMenu : MonoBehaviour
     public GameObject confirmationContainer;
     public Animator animator;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
