46cc0a1bb1d802d603bd499f34505eb610ba8eaa
Lucas Spits <spits.lucas@protonmail.com>
2021-05-31 11:49:30 +0200

    Feat: Audio settings are saveable, load settings on level start

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -8,11 +7,21 @@ public class SettingsMenu : MonoBehaviour
     public GameObject settingsMenuUI;
     public GameObject pauseMenuButtonContainer;
 
+    /// <summary>
+    /// The name of the exposed property of the master mixer
+    /// </summary>
     private const string MasterVolume = "MasterVolume";
+
+    private void Start()
+    {
+        // Restore the volume from global settings
+        audioMixer.SetFloat(MasterVolume, AudioPreferences.MasterVolume);
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat(MasterVolume, volume);
+        AudioPreferences.MasterVolume = volume;
     }
 
     public void LoadPauseMenu()
---
+++ Assets/Scripts/Preferences/AudioPreferences.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Global preferences for audio volume, setting the properties will automatically persist
+/// </summary>
+public static class AudioPreferences
+{
+    private const string MasterVolumePrefKey = "MasterVolume";
+    private const string MusicVolumePrefKey = "MusicVolume";
+    private const string SFXVolumePrefKey = "SFXVolume";
+
+    public static float MasterVolume
+    {
+        get => PlayerPrefs.GetFloat(MasterVolumePrefKey, 0);
+        set => PlayerPrefs.SetFloat(MasterVolumePrefKey, value);
+    }
+
+    public static float MusicVolume
+    {
+        get => PlayerPrefs.GetFloat(MusicVolumePrefKey, 0);
+        set => PlayerPrefs.SetFloat(MusicVolumePrefKey, value);
+    }
+
+    public static float SFXVolume
+    {
+        get => PlayerPrefs.GetFloat(SFXVolumePrefKey, 0);
+        set => PlayerPrefs.SetFloat(SFXVolumePrefKey, value);
+    }
+}
