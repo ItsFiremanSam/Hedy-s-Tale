3b94bd39383f0ed9a726a7fef85f98d84f699b58
Eva <evavanderscheer@live.nl>
2021-05-31 17:02:19 +0200

    Added collection pick up scripts and prefab
    The collection script is useful if you want to reward the player with a block after they pick up MULTIPLE items.

---
+++ Assets/Scripts/Interaction/CollectMultipleInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMultipleInteraction : InteractableObject
+{
+    public PCollectMultiple collection;
+
+    protected override void OnInteractWithPlayer(PlayerInteraction playerInteraction)
+    {
+        collection.CurrentAmount++;
+        if (collection.CurrentAmount == collection.Amount)
+        {
+                playerInteraction.Inventory.Add(collection.PuzzleBlock);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Interaction/PCollectMultiple.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCollectMultiple : MonoBehaviour
+{
+    public PuzzleBlock PuzzleBlock;
+    public int Amount;
+    public int CurrentAmount;
+
+}
