aedcd922878ffd2fda6fbe75f3cdf10b531e09f5
ItsFiremanSam <sbdutchey@gmail.com>
2021-06-04 16:26:19 +0200

    Added and implemented walking sounds to Hedy in level 2
    Sadly Hedy is not a prefab yet, so sounds need to be implemented in every level for the time being

---
+++ Assets/Scripts/Movement/WalkingSounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingSounds : MonoBehaviour
+{
+    public AudioClip[] stepSounds;
+    public AudioSource walkingSFXSource;
+
+    private void Step()
+    {
+        walkingSFXSource.PlayOneShot(GetRandomClip());
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return stepSounds[Random.Range(0, stepSounds.Length)];
+    }
+} 
