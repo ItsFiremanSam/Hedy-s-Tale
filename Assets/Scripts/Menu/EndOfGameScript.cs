e2920fab3e4d1b36bc18af45d3684abf64fd742b
ItsFiremanSam <sbdutchey@gmail.com>
2021-06-15 17:42:52 +0200

    Updated end of game screen
    End of game screen now has the same style, music and button sounds as the main menu.
    
    Credits can be added later on by adding an extra button.

---
+++ Assets/Scripts/Menu/EndOfGameScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndOfGameScript : MonoBehaviour
+{
+    public GameObject menuButtonContainer;
+    public GameObject confirmationContainer;
+
+    private const string OptionMainMenu = "MainMenu";
+    private const string OptionQuitGame = "QuitGame";
+
+    public void OpenConfirmationMenu(string option)
+    {
+        menuButtonContainer.SetActive(false);
+        confirmationContainer.SetActive(true);
+
+    GameObject yesButton = confirmationContainer.transform.GetChild(1).gameObject;
+
+        switch (option)
+        {
+            case OptionMainMenu:
+                yesButton.GetComponent<Button>().onClick.AddListener(LoadMainMenu);
+                break;
+            case OptionQuitGame:
+                yesButton.GetComponent<Button>().onClick.AddListener(QuitGame);
+                break;
+            default:
+                Debug.LogError("Your entered option does not match any of the available options!");
+                break;
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void No()
+    {
+        GameObject yesButton = confirmationContainer.transform.GetChild(1).gameObject;
+        yesButton.GetComponent<Button>().onClick.RemoveAllListeners();
+
+        confirmationContainer.SetActive(false);
+        menuButtonContainer.SetActive(true);
+    }
+
+}
