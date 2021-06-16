1a4cc93cbc47930f478e1c0351b289169502fe55
ItsFiremanSam <sbdutchey@gmail.com>
2021-06-16 23:08:53 +0200

    Added new script for end of level sign

---
+++ Assets/Scripts/Interaction/InteractionBeforeScenechange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractionBeforeScenechange : InteractableObject
+{
+    public Dialog dialog;
+
+    protected override void OnInteractWithPlayer(PlayerInteraction playerInteraction)
+    {
+        DialogManager dialogManager = DialogManager.Instance;
+        if (!dialogManager.DialogActive)
+            StartCoroutine(dialogManager.StartDialog(dialog));
+
+    }
+
+    private void ChangeSceneToLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    IEnumerator Coroutine()
+    {
+        PlayerMovement playerMovement = FindObjectOfType<PlayerMovement>();
+        playerMovement.DialogUIActive = true;
+
+        yield return new WaitForSeconds(.5f);
+
+        DialogManager dialogManager = DialogManager.Instance;
+        yield return dialogManager.StartDialog(dialog);
+    }
+}
